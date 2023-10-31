fbc9876857cf10ef917e570691d9ae9da9f77729
ciszko <mkalinski@plume.com>
2023-10-31 22:07:39 +0100

    Finish spells and items
    - move to articles from pages
    - implement custom Tag class
    - fix spells and items page
    - add task to copy the files and then serve
    - beautify everything

--- dnd_parser/dnd_parser.py
+++ dnd_parser/dnd_parser.py
@@ -1,8 +1,11 @@
+from datetime import datetime
 import re
 import yaml
+from pathlib import Path
 from pelican import signals
 from pelican.readers import BaseReader
 from pelican.utils import pelican_open
+from pelican.contents import Tag as BaseTag
 
 HEADER_RE = re.compile(
     r"\s*^---$"  # File starts with a line of "---" (preceeding blank lines accepted)
@@ -11,6 +14,13 @@ HEADER_RE = re.compile(
     r"(?P<content>.*)",
     re.MULTILINE | re.DOTALL,
 )
+
+
+class Tag(BaseTag):
+    def __init__(self, name, *args, display_name=None, type=None, **kwargs):
+        super().__init__(name, *args, **kwargs)
+        self.display_name = display_name if display_name else name
+        self.type = type
 
 
 # Create a new reader class, inheriting from the pelican.reader.BaseReader
@@ -28,17 +38,45 @@ class DnDReader(BaseReader):
         metadata = self.parse_yaml(m["metadata"], filename)
 
         if not (content := m["content"]):
-            content = metadata["opis"]
+            if metadata.get("content", None):
+                content = metadata["opis"]
+
+        if metadata.get("date", None) is None:
+            metadata["date"] = datetime(2023, 1, 1)
+
+        if metadata.get("title", None) is None:
+            metadata["title"] = metadata["nazwa"]
 
         return content, metadata
 
     def parse_yaml(self, metadata, filename):
         meta = yaml.safe_load(metadata)
+        f = Path(filename)
+        if not any(x in f.parts for x in ("czary", "przedmioty")):
+            return meta
         meta["title"] = meta["nazwa"]
-        if "spells" in filename:
+        if "czary" in filename:
             meta["template"] = "spell"
-        elif "items" in filename:
-            meta["cat"] = "item"
+            meta["tags"] = [
+                Tag(
+                    f"czar {x}",
+                    self.settings,
+                    display_name=x,
+                    type="class",
+                )
+                for x in meta["klasa"]
+            ]
+            meta["tags"].append(
+                Tag(
+                    f'krąg {meta["krąg"]}',
+                    self.settings,
+                    display_name=f"Krąg {meta['krąg']}",
+                    type="circle",
+                )
+            )
+        elif "przedmioty" in filename:
+            meta["template"] = "item"
+            meta["tags"] = [Tag(meta["rzadkość"], self.settings, type="rarity")]
         return meta
 
 
@@ -46,6 +84,5 @@ def add_reader(readers):
     readers.reader_classes["md"] = DnDReader
 
 
-# This is how pelican works.
 def register():
     signals.readers_init.connect(add_reader)
--- pelicanconf.py
+++ pelicanconf.py
@@ -2,15 +2,15 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).parent
 
-AUTHOR = 'Marcin'
-SITENAME = 'DnDHelp'
-SITEURL = ''
+AUTHOR = "Marcin"
+SITENAME = "DnDHelp"
+SITEURL = ""
 
-PATH = 'content'
+PATH = "content"
 
-TIMEZONE = 'Europe/Warsaw'
+TIMEZONE = "Europe/Warsaw"
 
-DEFAULT_LANG = 'Polish'
+DEFAULT_LANG = "Polish"
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
@@ -20,19 +20,29 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
 PLUGINS = ["dnd_parser.dnd_parser"]
-THEME = BASE_DIR / 'themes' / 'DnD'
+THEME = BASE_DIR / "themes" / "DnD"
+
+STATIC_PATHS = ["images", "extra"]
+
+EXTRA_PATH_METADATA = {
+    "extra/favicon.ico": {"path": "favicon.ico"},
+}
 
 # Blogroll
-LINKS = (('Pelican', 'https://getpelican.com/'),
-         ('Python.org', 'https://www.python.org/'),
-         ('Jinja2', 'https://palletsprojects.com/p/jinja/'),
-         ('You can modify those links in your config file', '#'),)
+LINKS = (
+    ("Pelican", "https://getpelican.com/"),
+    ("Python.org", "https://www.python.org/"),
+    ("Jinja2", "https://palletsprojects.com/p/jinja/"),
+    ("You can modify those links in your config file", "#"),
+)
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+SOCIAL = (
+    ("You can add links in your config file", "#"),
+    ("Another social link", "#"),
+)
 
 DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
-#RELATIVE_URLS = True
+# RELATIVE_URLS = True
